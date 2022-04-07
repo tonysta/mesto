e4a90aef2e38e04397b1eebcbb57ff3834105590
anton startsev <anton.startsev@osome.com>
2022-04-07 15:13:43 +0300

    add messages for validation

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,19 +1,24 @@
 const formElement = document.querySelector('.popup__form');
 const inputElement = formElement.querySelector('.popup__input');
+const inputError = formElement.querySelector(`.${inputElement.name}-input-error`);
 
-const showInputError = (element) => {
+const showInputError = (element, errorMessage) => {
     element.classList.add('popup__input_type_error');
+    inputError.textContent = errorMessage;
+    inputError.classList.add('popup__input-error_type_active');
 }
 
 const hideInputError = (element) => {
     element.classList.remove('popup__input_type_error');
+    inputError.classList.remove('popup__input-error_type_active');
+    inputError.textContent = '';
 }
 
 const checkInputValidity = () => {
     if(inputElement.validity.valid) {
         hideInputError(inputElement);
     } else {
-        showInputError(inputElement);
+        showInputError(inputElement, inputElement.validationMessage);
     }
 }
 
