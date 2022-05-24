b9bc246f7f081377ef47b16dccde0a74d0bcbca2
anton startsev <anton.startsev@osome.com>
2022-05-25 00:09:59 +0400

    add popups

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -7,9 +7,10 @@ export default class UserInfo {
     getUserInfo() {
         return {name: this._name.textContent, profession: this._profession.textContent};
     }
-    setUserInfo({name, about, avatar}) {
+    setUserInfo({name, about, avatar, _id}) {
         this._name.textContent = name;
         this._profession.textContent = about;
         this._avatar.src = avatar;
+        this._id = _id;
     }
 }
